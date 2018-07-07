aac712540dacca1f4860bc7d8e0892696c16aaa2
Shnifer <phoeni_ks@mail.ru>
2018-07-07 19:04:23 +0300

    +alice request

---
+++ alice/api.go
@@ -0,0 +1,77 @@
+package alice
+
+import (
+	"net/http"
+	"time"
+	"encoding/base64"
+	"bytes"
+	"encoding/json"
+	"github.com/pkg/errors"
+)
+
+var opts Opts
+var client *http.Client
+
+type Event struct{
+	EvType string `json:"eventType"`
+	Data [7]int `json:"data"`
+}
+
+type Events []Event
+
+type Opts struct{
+	Addr string
+	Path string
+	Login string
+	Password string
+
+	logPass64 string
+	url string
+}
+
+func InitAlice(initOpts Opts) {
+	opts = initOpts
+
+	b:=&bytes.Buffer{}
+	enc:=base64.NewEncoder(base64.StdEncoding,b)
+	enc.Write([]byte(opts.Login+":"+opts.Password))
+	enc.Close()
+
+	opts.logPass64 = b.String()
+	opts.url = opts.Addr+"/"+opts.Path+"/"
+
+	client = &http.Client{
+		Timeout: time.Second,
+	}
+}
+
+type reqData struct{
+	Events `json:"events"`
+}
+
+func DoReq(location string, events Events) error{
+
+	var data reqData
+	data.Events = events
+	body,err :=json.Marshal(data)
+	if err!=nil{
+		return err
+	}
+	bodyBuf :=bytes.NewBuffer(body)
+	req,err:=http.NewRequest(http.MethodPost, opts.url+location, bodyBuf)
+	if err!=nil{
+		return err
+	}
+	req.Header.Add("Accept","application/json")
+	req.Header.Add("Content-Type","application/json")
+	req.Header.Add("Authorization", "Basic "+opts.logPass64)
+	resp, err := client.Do(req)
+	if err!=nil{
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode!=200{
+		return errors.New(resp.Status)
+	}
+	return nil
+}
---
+++ execs/alice/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/Shnifer/magellan/alice"
+
+func main(){
+	opts:=alice.Opts{
+		Addr: "https://api.alice.magellan2018.ru",
+		Path: "location_events",
+		Password:"admin",
+		Login:"admin",
+	}
+	alice.InitAlice(opts)
+
+	events:=make(alice.Events,2)
+	events[0]=alice.Event{
+		EvType: "biological-systems-influence",
+		Data: [7]int{0,0,1,0,1,0,0},
+	}
+	events[1]=alice.Event{
+		EvType: "modify-nucleotide-instant",
+		Data: [7]int{0,-1,0,0,0,0,0},
+	}
+
+	err:=alice.DoReq("ship_1", events)
+	if err!=nil{
+		panic(err)
+	}
+}
